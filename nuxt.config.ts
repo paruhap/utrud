cd996ebaabe7dcf5ce0cbfb43657e10d274a4948
paruhap <paruhap@gmail.com>
2025-04-21 00:38:39 +0300

    удалил nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxthub/core', 'nuxt-auth-utils', '@nuxt/image'],
+  modules: ['nuxt-auth-utils', '@nuxt/image'],
   css: ['~/assets/css/main.css'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
@@ -27,42 +26,10 @@ export default defineNuxtConfig({
       groupId: process.env.TELEGRAM_GROUP_ID
     }
   },
-  routeRules: {
-    '/api/_hub/**': { cors: true }
-  },
-  security: {
-    headers: {
-      crossOriginEmbedderPolicy: false,
-      contentSecurityPolicy: false
-    }
-  },
-  nitro: {
-    preset: 'cloudflare-pages',
-    storage: {
-      db: {
-        driver: 'fs',
-        base: './.data/db'
-      }
-    },
-    devStorage: {
-      db: {
-        driver: 'fs',
-        base: './.data/db'
-      }
-    }
-  },
-  experimental: {
-    payloadExtraction: false,
-    inlineSSRStyles: false
-  },
   vite: {
-    build: {
-      target: 'esnext'
-    },
     server: {
       allowedHosts: [
-        '69f2-178-16-94-94.ngrok-free.app',
-        'admin.hub.nuxt.com'
+        '69f2-178-16-94-94.ngrok-free.app'
       ]
     }
   }
