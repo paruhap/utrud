e955ec470f36791a71788655f2908cb8426e2bf2
paruhap <paruhap@gmail.com>
2025-04-20 22:21:05 +0300

    третий коммит

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    'nuxt-auth-utils',
-    '@nuxt/image',
-  ],
+  modules: ['@nuxthub/core', 'nuxt-auth-utils', '@nuxt/image'],
   css: ['~/assets/css/main.css'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
@@ -30,10 +27,42 @@ export default defineNuxtConfig({
       groupId: process.env.TELEGRAM_GROUP_ID
     }
   },
+  routeRules: {
+    '/api/_hub/**': { cors: true }
+  },
+  security: {
+    headers: {
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: false
+    }
+  },
+  nitro: {
+    preset: 'cloudflare-pages',
+    storage: {
+      db: {
+        driver: 'fs',
+        base: './.data/db'
+      }
+    },
+    devStorage: {
+      db: {
+        driver: 'fs',
+        base: './.data/db'
+      }
+    }
+  },
+  experimental: {
+    payloadExtraction: false,
+    inlineSSRStyles: false
+  },
   vite: {
+    build: {
+      target: 'esnext'
+    },
     server: {
       allowedHosts: [
-        '69f2-178-16-94-94.ngrok-free.app'
+        '69f2-178-16-94-94.ngrok-free.app',
+        'admin.hub.nuxt.com'
       ]
     }
   }
